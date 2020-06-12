23949c43b5dd97571a44341c0c5902a6045f66ee
Veerendranath Jakkam <vjakkam@codeaurora.org>
2020-06-12 16:44:33 +0530

    hidl: Add support for FT SAE, FT FILS and FT Suite-B
    Enable FT-SAE, FT-FILS and FT-Suite-B AKMs in network profiles
    if target supports.
    
    CRs-Fixed: 2715328
    Change-Id: I594dc7b5d0a07c9f1a56cc265898a50671938c8f

--- wpa_supplicant/hidl/1.3/sta_network.cpp
+++ wpa_supplicant/hidl/1.3/sta_network.cpp
@@ -2471,6 +2471,31 @@ void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 		WPA_DRIVER_CAPA_KEY_MGMT_FT)) {
 		key_mgmt_mask |= WPA_KEY_MGMT_FT_IEEE8021X;
 	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_SAE) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_SAE)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_SAE;
+	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA256) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_FILS_SHA256)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_FILS_SHA256;
+	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA384) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_FILS_SHA384)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_FILS_SHA384;
+	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X_SUITE_B_192) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_802_1X_SHA384)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_IEEE8021X_SHA384;
+	}
+
 }
 
 /**
@@ -2485,6 +2510,22 @@ void StaNetwork::resetFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 
 	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X) {
 		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_IEEE8021X;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_SAE) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_SAE;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA256) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_FILS_SHA256;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA384) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_FILS_SHA384;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X_SUITE_B_192) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_IEEE8021X_SHA384;
 	}
 }
 
