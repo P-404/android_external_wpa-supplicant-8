5aa5f20639aa2ef853a334d0c746ff0c331ffc23
Veerendranath Jakkam <vjakkam@codeaurora.org>
2020-06-12 16:44:33 +0530

    hidl: Add support for FT SAE, FT FILS and FT Suite-B
    Enable FT-SAE, FT-FILS and FT-Suite-B AKMs in network profiles
    if target supports.
    
    CRs-Fixed: 2715328
    Change-Id: I594dc7b5d0a07c9f1a56cc265898a50671938c8f

--- wpa_supplicant/hidl/1.2/sta_network.cpp
+++ wpa_supplicant/hidl/1.2/sta_network.cpp
@@ -2152,6 +2152,13 @@ int StaNetwork::setByteArrayKeyFieldAndResetState(
  */
 void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 {
+	struct wpa_supplicant *wpa_s = retrieveIfacePtr();
+	struct wpa_driver_capa capa;
+
+	if (wpa_drv_get_capa(wpa_s, &capa) < 0) {
+		return;
+	}
+
 	if (key_mgmt_mask & WPA_KEY_MGMT_PSK) {
 		key_mgmt_mask |= WPA_KEY_MGMT_FT_PSK;
 	}
@@ -2159,6 +2166,31 @@ void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X) {
 		key_mgmt_mask |= WPA_KEY_MGMT_FT_IEEE8021X;
 	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_SAE) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_SAE)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_SAE;
+	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA256) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_FILS_SHA256)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_FILS_SHA256;
+	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA384) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_FILS_SHA384)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_FILS_SHA384;
+	}
+
+	if ((key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X_SUITE_B_192) &&
+	    (capa.key_mgmt_iftype[WPA_IF_STATION] &
+		WPA_DRIVER_CAPA_KEY_MGMT_FT_802_1X_SHA384)) {
+		key_mgmt_mask |= WPA_KEY_MGMT_FT_IEEE8021X_SHA384;
+	}
+
 }
 
 /**
@@ -2173,6 +2205,22 @@ void StaNetwork::resetFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 
 	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X) {
 		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_IEEE8021X;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_SAE) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_SAE;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA256) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_FILS_SHA256;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_FILS_SHA384) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_FILS_SHA384;
+	}
+
+	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X_SUITE_B_192) {
+		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_IEEE8021X_SHA384;
 	}
 }
 }  // namespace implementation
