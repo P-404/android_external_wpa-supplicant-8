5b48a9d9cee1b70cb47dca79345614ed37536866
Srilakshmi Kanagalla <kanagall@codeaurora.org>
2020-06-24 12:46:24 +0530

    Enable SAE for AP mode across all the targets.
    SAE is getting mandatory for all the targets. Hence enabling the same
    in wpa-supplicant for all the targets.
    
    Change-Id: I812bd564702cfca35fb5e8aede154c780dcf96d9
    CRs-Fixed: 2722874

--- hostapd/Android.mk
+++ hostapd/Android.mk
@@ -20,7 +20,6 @@ ifeq ($(call is-board-platform-in-list,msm8909 msm8937 msm8953 msm8996 msm8998 s
   $(warning "Disabling WPA3 support in hostapd for $(TARGET_BOARD_PLATFORM)")
   CONFIG_OWE=n
   CONFIG_DPP=n
-  CONFIG_SAE=n
   CONFIG_SUITEB192=n
 endif
 
