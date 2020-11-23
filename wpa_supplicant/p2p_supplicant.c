dc26c5eb35252843cfc52e0267ad90421bb8cc2e
Sreeramya Soratkal <ssramya@codeaurora.org>
2020-11-23 13:30:02 +0530

    Add support for new 5GHz channels 173 and 177
    Add support for new channels 173 and 177 in the operating classes 125 to
    130 as defined in draft IEEE P802.11ax/D7.0.
    
    CRs-fixed: 2862030
    Git-repo: git://w1.fi/srv/git/hostap.git
    Git-commit: 833cdbe97dd4a7c6c225a5fc0ac2eceb0799c9d8
    Change-Id: I6410d23881e6cdd6f9df2aacde6aa132c38cb21c
    Signed-off-by: Sreeramya Soratkal <ssramya@codeaurora.org>

--- wpa_supplicant/p2p_supplicant.c
+++ wpa_supplicant/p2p_supplicant.c
@@ -3568,7 +3568,7 @@ static int wpas_p2p_get_center_80mhz(struct wpa_supplicant *wpa_s,
 				     struct hostapd_hw_modes *mode,
 				     u8 channel)
 {
-	u8 center_channels[] = { 42, 58, 106, 122, 138, 155 };
+	u8 center_channels[] = { 42, 58, 106, 122, 138, 155, 171 };
 	size_t i;
 
 	if (mode->mode != HOSTAPD_MODE_IEEE80211A)
@@ -3629,7 +3629,7 @@ static int wpas_p2p_get_center_160mhz(struct wpa_supplicant *wpa_s,
 				     struct hostapd_hw_modes *mode,
 				     u8 channel)
 {
-	u8 center_channels[] = { 50, 114 };
+	u8 center_channels[] = { 50, 114, 163 };
 	unsigned int i;
 
 	if (mode->mode != HOSTAPD_MODE_IEEE80211A)
@@ -3777,7 +3777,7 @@ static int wpas_p2p_setup_channels(struct wpa_supplicant *wpa_s,
 
 			/* Check for non-continuous jump in channel index
 			 * incrementation */
-			if ((o->op_class == 128 || o->op_class == 130) &&
+			if ((o->op_class >= 128 && o->op_class <= 130) &&
 			    ch < 149 && ch + o->inc > 149)
 				ch = 149;
 
