af82b371f18cdf310a99159573470a7fecfad2ba
Vinita S. Maloo <quic_vmaloo@quicinc.com>
2021-04-12 16:56:24 +0530

    Allow MSCS support to be disabled for testing purposes
    "SET disable_mscs_support 1" can be used to disable indication of MSCS
    support in the Extended Capabilities element for testing purposes. This
    is also disabling addition of the MSCS element even if valid
    configuration parameters had been configured.
    
    Change-Id: I149a68a174b9709af1fa2427786e9d6854144d6e
    CRs-fixed: 3020694
    Git-repo: git://w1.fi/srv/git/hostap.git
    Git-commit: e433d06dd5cc904dcffff4f3d95b43fc4283dbe3
    Signed-off-by: Vinita S. Maloo <vmaloo@codeaurora.org>

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -797,6 +797,8 @@ static int wpa_supplicant_ctrl_iface_set(struct wpa_supplicant *wpa_s,
 		ret = wpas_ctrl_iface_set_dso(wpa_s, value);
 	} else if (os_strcasecmp(cmd, "disable_scs_support") == 0) {
 		wpa_s->disable_scs_support = !!atoi(value);
+	} else if (os_strcasecmp(cmd, "disable_mscs_support") == 0) {
+		wpa_s->disable_mscs_support = !!atoi(value);
 #ifdef CONFIG_DPP
 	} else if (os_strcasecmp(cmd, "dpp_config_obj_override") == 0) {
 		os_free(wpa_s->dpp_config_obj_override);
@@ -8422,6 +8424,7 @@ static void wpa_supplicant_ctrl_iface_flush(struct wpa_supplicant *wpa_s)
 	wpa_s->rsnxe_override_eapol = NULL;
 	wpas_clear_driver_signal_override(wpa_s);
 	wpa_s->disable_scs_support = 0;
+	wpa_s->disable_mscs_support = 0;
 #ifdef CONFIG_DPP
 	os_free(wpa_s->dpp_config_obj_override);
 	wpa_s->dpp_config_obj_override = NULL;
