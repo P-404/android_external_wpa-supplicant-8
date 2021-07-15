0670dc1368bbcea5597036397d40b725276d393c
Veerendranath Jakkam <vjakkam@codeaurora.org>
2021-07-15 11:09:33 +0530

    hidl: Set external flag for PMKSA entries configured from framework
    Set external flag for PMKSA entries configured from framework to
    wpa_supplicant to differentiate them from internally generated PMKSA
    entries.
    
    Change-Id: I3aef8be3bd957995415c9f18b3dbb57db5ccd60a
    CRs-Fixed: 2974692

--- wpa_supplicant/hidl/1.4/sta_network.cpp
+++ wpa_supplicant/hidl/1.4/sta_network.cpp
@@ -2279,6 +2279,7 @@ SupplicantStatus StaNetwork::setPmkCacheInternal(const std::vector<uint8_t>& ser
 	ss.write((char *) serializedEntry.data(), std::streamsize(serializedEntry.size()));
 	misc_utils::deserializePmkCacheEntry(ss, new_entry);
 	new_entry->network_ctx = wpa_ssid;
+	new_entry->external = true;
 	wpa_sm_pmksa_cache_add_entry(wpa_s->wpa, new_entry);
 
 	return {SupplicantStatusCode::SUCCESS, ""};
