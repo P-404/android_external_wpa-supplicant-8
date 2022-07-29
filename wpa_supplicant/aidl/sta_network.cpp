a30c54a8978807a6e39584dd092566e45b69deff
Narasimha Rao PVS <quic_npvs@quicinc.com>
2022-07-29 17:53:31 +0530

    Enable driver based pmk cache in FT-EAP/SUITEB
    This commit introduces to Enable driver based PMK cache
    for FT-EAP and FT-SUITEB key_mgmt.
    
    Change-Id: Id473904c20829d433025966cbae35b1a60eb35e2
    CRs-Fixed: 3256525

--- wpa_supplicant/aidl/sta_network.cpp
+++ wpa_supplicant/aidl/sta_network.cpp
@@ -2474,6 +2474,7 @@ int StaNetwork::setByteArrayKeyFieldAndResetState(
 void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 {
 	struct wpa_supplicant *wpa_s = retrieveIfacePtr();
+	struct wpa_ssid *wpa_ssid = retrieveNetworkPtr();
 	int res;
 	struct wpa_driver_capa capa;
 
@@ -2483,6 +2484,7 @@ void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 
 	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X) {
 		key_mgmt_mask |= WPA_KEY_MGMT_FT_IEEE8021X;
+		wpa_ssid->ft_eap_pmksa_caching = 1;
 	}
 
 	res = wpa_drv_get_capa(wpa_s, &capa);
@@ -2512,6 +2514,7 @@ void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 		    (capa.key_mgmt_iftype[WPA_IF_STATION] &
 			WPA_DRIVER_CAPA_KEY_MGMT_FT_802_1X_SHA384)) {
 			key_mgmt_mask |= WPA_KEY_MGMT_FT_IEEE8021X_SHA384;
+			wpa_ssid->ft_eap_pmksa_caching = 1;
 		}
 #endif
 #endif
@@ -2525,12 +2528,15 @@ void StaNetwork::setFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
  */
 void StaNetwork::resetFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 {
+	struct wpa_ssid *wpa_ssid = retrieveNetworkPtr();
+
 	if (key_mgmt_mask & WPA_KEY_MGMT_PSK) {
 		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_PSK;
 	}
 
 	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X) {
 		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_IEEE8021X;
+		wpa_ssid->ft_eap_pmksa_caching = 0;
 	}
 #ifdef CONFIG_IEEE80211R
 #ifdef CONFIG_SAE
@@ -2550,6 +2556,7 @@ void StaNetwork::resetFastTransitionKeyMgmt(uint32_t &key_mgmt_mask)
 #ifdef CONFIG_SUITEB192
 	if (key_mgmt_mask & WPA_KEY_MGMT_IEEE8021X_SUITE_B_192) {
 		key_mgmt_mask &= ~WPA_KEY_MGMT_FT_IEEE8021X_SHA384;
+		wpa_ssid->ft_eap_pmksa_caching = 0;
 	}
 #endif
 #endif
