32ade222515e5e4510cf5045ecb61eb508de5651
Veerendranath Jakkam <quic_vjakkam@quicinc.com>
2022-09-08 20:14:08 +0530

    Add a helper function to get ML IE of specified type from IEs buffer
    Add a helper function to find the specified type of Multi-Link element
    from IEs buffer.
    
    Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>
    CRs-Fixed: 3294072
    Git-repo: git://w1.fi/hostap.git
    Git-commit: bd499f0e6e60142244732a747b2b8527f06eebaf
    Change-Id: Iff7fcfa8d2fb474c538c5fa90475c2f3ecac6a38

--- src/common/ieee802_11_common.c
+++ src/common/ieee802_11_common.c
@@ -3090,3 +3090,20 @@ enum chan_width get_sta_operation_chan_width(
 			? CHAN_WIDTH_80P80 : CHAN_WIDTH_80;
 	return ap_operation_chan_width;
 }
+
+
+const u8 * get_ml_ie(const u8 *ies, size_t len, u8 type)
+{
+	const struct element *elem;
+
+	if (!ies)
+		return NULL;
+
+	for_each_element_extid(elem, WLAN_EID_EXT_MULTI_LINK, ies, len) {
+		if (elem->datalen >= 2 &&
+		    (elem->data[1] & MULTI_LINK_CONTROL_TYPE_MASK) == type)
+			return &elem->id;
+	}
+
+	return NULL;
+}
